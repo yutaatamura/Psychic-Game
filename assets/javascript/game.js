51098078898998f2f565db29235cd9df1a2a945e
unknown <yutaatamura@gmail.com>
2018-02-11 16:27:49 -0600

    Basic javascript outline for psychic game.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,82 @@
-   var userScore = 0; 
-   var computerScore = 0;
+   var userWins = 0; 
+   var userLosses = 0;
+   var guesses = 9;
    var lettersGuessed = [];
+   var computerOutput;
+   var userInputLetterGlobal;
+   var double = "false";
     
     
     var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-    var computerOutput = letters[Math.floor(Math.random() * letters.length)];
-    //console.log(computerOutput);
+    document.getElementById("guessesLeft").innerHTML = guesses;
+
+    document.onload = generateRandomLetter();
+
+    //generates random number from 0 to letters.length (26), and selects index in 'letters' array
+    function generateRandomLetter () {
+
+    computerOutput = letters[Math.floor(Math.random() * letters.length)];
+    console.log(computerOutput);
+    };
+
+    function update() {
+        document.getElementById("userLosses").innerHTML = userLosses; 
+        document.getElementById("userWins").innerHTML = userWins;
+        document.getElementById("guessesLeft").innerHTML = guesses; 
+        document.getElementById("guessedLetters").innerHTML = lettersGuessed;
+    };
 
    function reset() {
+       lettersGuessed = [];
+       guesses = 9;
+       update();
+       generateRandomLetter();
+   };
 
-   }
+   function checkDouble() {
+    for (var i = 0; i < lettersGuessed.length; i++) {
+        for (var j = i + 1; j < lettersGuessed.length; j++) {
+            console.log("J loop");    
+            if (lettersGuessed[i] === lettersGuessed[j] && i != j) {
+                lettersGuessed.splice(i, 1);
+                alert("You have already guessed this letter, try again.");
+                double === "true";
+            }
+        }; 
+    };
+};  
 
+   function compare() {
+        if (double === "true") {
+            return;
+        } else if 
+    
+        (userInputLetterGlobal === computerOutput) {
+            alert("Correct! You guessed it right!");
+            userWins++;
+            reset();
+            update();
+        
+        } else {
+            alert("WRONG! Try again!");
+            guesses--;
+            if (guesses === 0) {
+                userLosses++
+                reset();
+            };
+            update();  
+        }
+    };
+
+
+   //on key up, gets event.which ASCII number and converts to a string letter using fromCharCode. Converts any capital letters to lower case.
 document.onkeyup = function(event) {
 
     var userInputLetter = String.fromCharCode(event.which).toLowerCase();
+    userInputLetterGlobal = userInputLetter;
+    // pushes userInputLetter into the array lettersGuessed
     lettersGuessed.push(userInputLetter);
-    document.getElementById("guessedLetters").innerHTML = "Your guesses so far: " + lettersGuessed.join(', ');
-
-
-    if (userInputLetter === computerOutput) {
-        alert("You guessed it!");
-        userScore++;
-        document.getElementById("userScore").innerHTML = userScore;
-        
-    } else {
-        alert("BITCH, you STUPID. Try again.... smh.");
-        computerScore++;
-        document.getElementById("computerScore").innerHTML = computerScore;
-        
-        //alert("WRONG! Try again!");
-    }
-        
-    
+    checkDouble();
+    compare();
 };
-
-
-
-
-
-
-
